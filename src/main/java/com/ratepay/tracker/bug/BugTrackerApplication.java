90421f25d8632aad6e22c5bb9f28b63e2ce14161
Your Name <arun@Penguin.local>
2022-11-11 17:35:05 +0100

    Minor update

--- src/main/java/com/ratepay/tracker/bug/BugTrackerApplication.java
+++ src/main/java/com/ratepay/tracker/bug/BugTrackerApplication.java
@@ -1,13 +1,16 @@
 package com.ratepay.tracker.bug;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableMongoRepositories
+@ImportAutoConfiguration(exclude = EmbeddedMongoAutoConfiguration.class)
 public class BugTrackerApplication {
 
 	public static void main(String[] args) {
